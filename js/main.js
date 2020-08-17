7712cec5e1bd743be2cdc0db4fe6583db75b8041
Eric Ricky <ericricky200@gmail.com>
2020-08-17 08:04:42 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,55 @@
+
+$(document).ready(function(){
+    // smooth scrolling
+    $(".navbar a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function(){
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+
+    // slide in
+    $(window).scroll(function() {
+        $(".slideanim").each(function(){
+          var pos = $(this).offset().top;      
+          var winTop = $(window).scrollTop();
+          if (pos < winTop + 600) {
+            $(this).addClass("slide");
+          }
+        });
+    });
+
+    // navbar onscroll
+    $(window).on('scroll', function() {
+        let pixels = 50;
+        if ($(window).scrollTop() > pixels) {
+            $('.navbar-expand-md').addClass('navbar-reduce');
+            $('.navbar-expand-md').removeClass('navbar-transparent');
+          } else {
+            $('.navbar-expand-md').addClass('navbar-transparent');
+            $('.navbar-expand-md').removeClass('navbar-reduce');
+          }
+    });
+
+    // number count animation
+    $('.count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 5000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+    
+  })
